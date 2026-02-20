4786c30ad4c687645b36f31a78803b454a9a84a1
ArashBahadori <arash.bhd.1385@gmail.com>
2026-02-21 00:06:33 +0330

    Arash - Blocks struct forms

---
+++ cmake-build-debug/CMakeFiles/project6.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project6.dir/main.cpp.obj"
+  "CMakeFiles/project6.dir/main.cpp.obj.d"
+  "libproject6.dll.a"
+  "project6.exe"
+  "project6.exe.manifest"
+  "project6.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
